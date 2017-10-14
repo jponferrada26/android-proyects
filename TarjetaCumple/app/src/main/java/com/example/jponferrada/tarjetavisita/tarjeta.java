0575a73771530aa9ea0c800ab0bf8024085bf6d4
Javier Ponferrada López <jponferrada@MacBook-Pro-de-Javier.local>
2017-10-14 13:52:09 +0200

    upload all proyects

---
+++ TarjetaCumple/app/src/main/java/com/example/jponferrada/tarjetavisita/tarjeta.java
@@ -0,0 +1,13 @@
+package com.example.jponferrada.tarjetavisita;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tarjeta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tarjeta);
+    }
+}
